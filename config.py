3bed7670034a8860fbf18a6eb8ae8a10ac6c3ba0
rif-zu <74507844+rif-zu@users.noreply.github.com>
2021-01-11 20:39:48 +0000

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-NS = 'http://www.tentex.io/data/news'
-
-INPUT_DATA_DIR='/home/ubuntu/data'
-OUTPUT_DATA_DIR='/home/ubuntu/triples'
-
-# The credentials of the elastic search server
-ES_USERNAME = 'elastic'
-ES_PASSWORD = 'changeme'
-ES_ENDPOINT = 'http://localhost:9200'
